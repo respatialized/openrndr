fe3cfe4345800bac7d7dfe87288fdecc0ddcf6ee
Andrew Foltz-Morrison <afoltzm@gmail.com>
2021-10-23 19:31:18 -0400

    Controlled start and stop

--- src/respatialized/openrndr.clj
+++ src/respatialized/openrndr.clj
@@ -1,13 +1,43 @@
 (ns respatialized.openrndr
   (:import [org.openrndr Application Program Configuration]
-           [org.openrndr.shape Circle])
+           [org.openrndr.shape Circle]
+           [org.openrndr.color ColorHSVa ColorRGBa ])
   (:gen-class))
 
+(def config
+  (doto (Configuration. )
+    (.setTitle "RESPATIALIZED // openrndr")
+    (.setWidth 900)
+    (.setHeight 900)
+    ))
+
+(def color
+  (ColorHSVa/Companion))
+
+
 (comment
-  (.run (Application/Companion) (Program.) (Configuration.))
+  (ColorHSVa. 100.0 100.0 100.0 100.0))
+
+(def program
+  (doto (Program. )
+    (.setBackgroundColor
+     (.toRGBa (ColorHSVa. 1.0 0.7 0.5 100.0))
+     )))
+
+(defn launch [] (.run (Application/Companion) program config))
+
+(def app
+  (doto
+      (Thread. launch)
+      (.setDaemon true)
+      (.start)))
+
+(comment
+
+  (.stop app)
+
 
   )
-
 
 (defn greet
   "Callable entry point to the application."
